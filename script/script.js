aeb49bd5d9375e00a11d01fceb9bb6d19e0689a1
Hussain Vahora <101166530+Hussain-1303@users.noreply.github.com>
2024-09-17 17:55:46 -0400

    Successfully Fetched Verse From RapidAPI
    Successfully Fetched Verse From Geeta's RapidAPI -Hussain

---
+++ script/script.js
@@ -0,0 +1,32 @@
+let verse = document.querySelector(".geeta .verse")
+
+
+
+//
+// async function getVerse() {
+//     const response = await fetch(`https://bhagavad-gita3.p.rapidapi.com/v2/chapters/1/verses/1/`)
+//     const data = await response.json()
+//     let textVerse = document.createTextNode(data.data.text)
+//     verse.appendChild(textVerse)
+// }
+
+// getVerse()
+
+
+const url = 'https://bhagavad-gita3.p.rapidapi.com/v2/chapters/2/verses/1/';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '056ddf58bcmsh12ac36ed2dd0e91p1ed8d7jsnfeeaf757d90d',
+		'x-rapidapi-host': 'bhagavad-gita3.p.rapidapi.com'
+	}
+};
+
+async function getVerse() {
+    const response = await fetch(url, options);
+    const data = await response.json(); // Parse the JSON response
+    let textVerse = document.createTextNode(data.text);
+    verse.appendChild(textVerse);
+}
+
+getVerse();
