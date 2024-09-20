d2d73bf94338b2c57a1d87c7edba1a1c94059cb7
Hussain Vahora <101166530+Hussain-1303@users.noreply.github.com>
2024-09-20 15:40:42 -0400

    Added Greetings, Date and Time
    Static greeting is added, Dynamic date and time feature added for aesthetic user interaction.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,32 @@
+//date
+
+const d = new Date();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let month = months[d.getMonth()];
+let dateElemnet = document.querySelector(".date .current-date")
+dateElemnet.textContent =  `${d.getDate()}, ${month} ${d.getFullYear()}`
+
+// time
+
+let timeElement = document.querySelector('.date .time')
+
+
+function time() {
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    timeElement.textContent = 
+      ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+  }
+  
+  setInterval(time, 1000);
+
+
+//verse
+
 let verse = document.querySelector(".geeta .verse")
-
-
 
 //
 // async function getVerse() {
@@ -30,3 +56,20 @@ async function getVerse() {
 }
 
 getVerse();
+
+//Greeting 
+
+let good = document.querySelector(".hello")
+let word = ""
+
+if (d.getHours() < 12) {
+    word = "Good Morning, "
+} else if (d.getHours() < 18) {
+    word = "Good Afternoon, "
+} else if (d.getHours() < 20) {
+    word = "Good Evinning, "
+} else if (d.getHours() < 24) {
+    word = "Good Night, "
+}
+
+good.textContent = word
